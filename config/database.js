9038d11b3f2aeb5994edac0096095bb086c35d10
Aswun <aswinlutfian32@gmail.com>
2026-01-31 18:55:47 +0700

    Fix database connection credentials in Sequelize configuration

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('library_geo_db', 'root', '', {
+const sequelize = new Sequelize('library_geo_db', 'root', '1234ag56', {
     host: 'localhost',
+    port: 3307,
     dialect: 'mysql',
     logging: false
 });
